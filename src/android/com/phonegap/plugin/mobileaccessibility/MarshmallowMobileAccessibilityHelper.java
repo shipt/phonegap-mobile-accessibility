ea63ea4c76dee4bde3869713eeeffd67fa6baacc
Phillip <key.phillip@gmail.com>
2018-03-25 20:25:20 -0500

    create files for andriod 6.0 and 7.0

---
+++ src/android/com/phonegap/plugin/mobileaccessibility/MarshmallowMobileAccessibilityHelper.java
@@ -0,0 +1,19 @@
+package com.phonegap.plugin.mobileaccessibility;
+
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.view.accessibility.AccessibilityManager.TouchExplorationStateChangeListener;
+import android.view.accessibility.CaptioningManager;
+import android.view.accessibility.CaptioningManager.CaptioningChangeListener;
+
+@TargetApi(Build.VERSION_CODES.M)
+public class MarshmallowMobileAccessibilityHelper extends
+    KitKatMobileAccessibilityHelper {
+
+        @Override
+        public void initialize(MobileAccessibility mobileAccessibility) {
+            super.initialize(mobileAccessibility);
+            mParent = mView.getParentForAccessibility();
+        }
+    }
